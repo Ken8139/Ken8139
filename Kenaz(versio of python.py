af1395754bf4ec32ff2d2535bce8a3e27f254fe8
Kenaz Mathukutty <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-11-11 22:53:59 +0530

    Add files via upload

---
+++ Kenaz(versio of python.py
@@ -0,0 +1,8 @@
+'''
+Python program to find user details
+author:Kenaz mathakutty
+Created:10/11/2024
+'''
+User_name=input("What is your name?")
+User_age=input("How old are you?")
+print("Hello,",User_name,"! You're",User_age,"Years old")
